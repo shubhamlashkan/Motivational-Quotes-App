42f1dbf698d5c82e1ac2cfa2bae3029c10edcbb1
shubhamlashkan <shubhamlashkan@gmail.com>
2023-07-08 11:19:40 +0530

    Added Tweet Link

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 const dataContainer = document.getElementById('cardContainer');
 fetch('https://type.fit/api/quotes')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-     data.forEach(item => {
-      const card = document.createElement('div');
-      card.classList.add('card');
+.then(response=>response.json())
+.then(data=>{
+    //console.log(data)
+    data.forEach(item=>{
+        //console.log(item.text );
+        //console.log(item.author);
+        if(item.author!=null){
+            const card = document.createElement('div')
+        card.classList.add('card');
 
-      const nameElement = document.createElement('h2');
-      nameElement.textContent = item.text;
-      card.appendChild(nameElement);
+        const nameElement = document.createElement('h2');
+        nameElement.textContent = item.text;
+        card.appendChild(nameElement);
 
-      if(item.author!=null){
-        const ageElement = document.createElement('p');
-        ageElement.textContent = `Author: ${item.author}`;
-        card.appendChild(ageElement);
-      }
+        const authorElement = document.createElement('p');
+        authorElement.textContent = item.author;
+        card.appendChild(authorElement);
 
-      var a = document.createElement('a');
-      var link = document.createTextNode("Tweet this");
-      a.appendChild(link);
-      a.title = "Tweet this";
-      a.id = 'tweetLink';
-      a.target='_blank';
-      a.style.float = 'right';
-      a.style.textDecoration = 'none';
-      a.style.fontSize = '1rem';
-      var finaltext = item.text + '%0A%0A'+'More Quotes => '+'%0A%0A'+'Developed By: @shubhamlashkan';
-      a.href = 'https://twitter.com/intent/tweet?text='+finaltext;
-      card.appendChild(a);
+        const a = document.createElement('a')
+        const link = document.createTextNode('Tweet this');
+        a.appendChild(link);
+        a.title = 'Tweet This';
+        a.target="_blank";
+        a.style.float = 'right';
+        a.style.textDecoration='none';
+        a.style.fontSize='1rem';
+        var finaltext = item.text + '%0A%0A' + 'Developed by: @shubhamlashkan';
+        a.href='https://twitter.com/intent/tweet?text='+finaltext;
 
-      dataContainer.appendChild(card);
-    });
-  })
-  .catch(error => console.error(error));
+        card.appendChild(a);
+        dataContainer.appendChild(card);
+        }
+        
+
+
+    })
+}).catch(error=>
+    console.error(error));
